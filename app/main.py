f95dd2e165b148d2f65926195d3e67ae73678cdc
mythu <thutrinhthimythu@gmail.com>
2026-07-26 16:36:17 +0700

    Fix CORS config for production

--- app/main.py
+++ app/main.py
@@ -17,7 +17,7 @@ Base.metadata.create_all(bind=engine)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origin_regex=".*",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
